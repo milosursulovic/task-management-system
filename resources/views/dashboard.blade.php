8821ece1ffd07b7832a692237777fbd6ad9a9d12
Misa <milosursulovic2696@gmail.com>
2023-04-23 13:28:39 +0200

    Implement design for create by me

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,38 @@
 @extends('main')
 
 @section('container')
-    dashboard
+    @if (Auth::user()->role == 'admin')
+    @else
+        <h1>{{ __('Example') }}</h1>
+        <div class="dashboard-container">
+            <div class="dashboard-options">
+                <button class="btn">{{ __('Created by me') }}</button>
+                <button class="btn">{{ __('Assigned to me') }}</button>
+                <button class="btn">{{ __('All Tasks') }}</button>
+            </div>
+            <div class="dashboard-list">
+                <div class="dashboard-list-item">
+                    <p class="dashboard-list-item-left">Item 1</p>
+                    <div class="dashboard-list-item-right">
+                        <p>Assigned To: Test 2</p>
+                        <i class="fas fa-pen"></i>
+                    </div>
+                </div>
+                <div class="dashboard-list-item">
+                    <p class="dashboard-list-item-left">Item 2</p>
+                    <div class="dashboard-list-item-right">
+                        <p>Assigned To: Test 3</p>
+                        <i class="fas fa-pen"></i>
+                    </div>
+                </div>
+                <div class="dashboard-list-item">
+                    <p class="dashboard-list-item-left">Item 3</p>
+                    <div class="dashboard-list-item-right">
+                        <p>Assigned To: Test 4</p>
+                        <i class="fas fa-pen"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,13 +4,19 @@
 <head>
     <title>{{ $title }}</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     @if (Auth::check())
         <form id="logout-form" action="{{ route('logout') }}" method="POST">
             @csrf
-            <button class="btn" type="submit">Logout</button>
+            <div class="inner-logout">
+                <h3>{{ Auth::user()->email }}</h3>
+                <button class="btn" type="submit">Logout</button>
+            </div>
         </form>
     @endif
     <div class="container">
